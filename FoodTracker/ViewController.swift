d704d66883e4cefad61059ca84e29fdb4c3ea472
Matt Roberts <mattroberts@Matts-MacBook-Pro.local>
2018-04-04 10:50:41 +0100

    First commit with Git

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Matt Roberts on 03/04/2018.
+//  Copyright © 2018 Matt Roberts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet var buttonLabel: UILabel!
+
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+  
+  @IBAction func buttonPushed() {
+  
+  }
+
+}
+
