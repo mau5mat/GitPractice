77a54fa05df0097e4f8eb6696a58a4c9ddf5ffb9
Matt Roberts <mattroberts@Matts-MacBook-Pro.local>
2018-04-05 16:25:00 +0100

    Implemented logic for button that changes text of label, added photo to assests and created a UIImageView for it to be displayed in

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,19 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  @IBOutlet var buttonLabel: UILabel!
-
+class ViewController: UIViewController, UITextFieldDelegate {
+  
+  //MARK: Properties
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var mealNameLabel: UILabel!
+  
   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+      // Handle the text field’s user input through delegate callbacks.
+      nameTextField.delegate = self
     }
   
-  @IBAction func buttonPushed() {
-  
+  //MARK: UITextFieldDelegate
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // Hide the keyboard
+    textField.resignFirstResponder()
+    return true
   }
-
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    mealNameLabel.text = textField.text
+  }
+  
+  //MARK: Actions
+  @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    mealNameLabel.text = "Default Text"
+  }
+  
 }
 
