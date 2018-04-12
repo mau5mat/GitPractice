ae601f153ad61478cec7549b35b5a963afffe722
Matt Roberts <mattroberts@Matts-MacBook-Pro.local>
2018-04-12 15:38:42 +0100

    Started implementing basic navigation functionality with navigation controllers and bar button item objects

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Matt Roberts on 03/04/2018.
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
   //MARK: Properties
   @IBOutlet weak var nameTextField: UITextField!
-  @IBOutlet weak var mealNameLabel: UILabel!
   @IBOutlet weak var photoImageView: UIImageView!
   @IBOutlet weak var ratingControl: RatingControl!
   
@@ -32,7 +31,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
   }
   
   func textFieldDidEndEditing(_ textField: UITextField) {
-    mealNameLabel.text = textField.text
+    
   }
   
   //MARK: UIImagePickerControlDelegate
